6872e708cf8f4893e6c9a2f7ed0dec26979c75a3
huhq <fox2081@foxmail.com>
2018-08-30 17:55:09 +0800

    [Add]: Add new components: im-modal.vue

---
+++ src/components/dir/im-modal.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="im-modal scale-in" v-bind:class="true" v-show="false">
+    <div class="bg-mask" v-show="true"></div>
+    <div class="modal-main">
+      <div class="ml-header" v-show="dialog.s.header">
+        <span class="title" v-show="dialog.title || '温馨提示'"></span>
+        <a class="close" @click="close()" v-show="dialog.s.close">×</a>
+      </div>
+      <div class="ml-body" v-show="dialog.s.body"></div>
+      <div class="ml-footer" v-show="dialog.s.footer">
+        <button class="ok" @click="ok()" v-bind="dialog.okBtn" v-show="dialog.hideBtn.ok"></button>
+        <button class="cancel" @click="cancel()" v-bind="dialog.cancelBtn || '取消'"
+                v-show="dialog.hideBtn.cancel"></button>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'im-modal',
+  data () {
+    return {
+      dialog: {
+        okBtn: '确定',
+        hideBtn: {},
+        s: {}
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
